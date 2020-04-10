a6a0c6b1d841ed5ee1bbe7a55f8d943440628c45
Han-Shen Yuan <han.yuan@gmail.com>
2020-04-10 14:08:37 -0700

    Updated specs for new usage

--- spec/reminders_spec.rb
+++ spec/reminders_spec.rb
@@ -7,10 +7,10 @@ describe Todoist::Sync::Reminders do
     config.cassette_library_dir = "fixtures/vcr_cassettes"
     config.hook_into :webmock
   end
-  
+
   before(:all) do
     Todoist::Util::Uuid.type = "reminders"
-  end  
+  end
 
   before do
     @client = load_client
@@ -23,28 +23,28 @@ describe Todoist::Sync::Reminders do
     end
   end
 
-  it "is able to add and update reminder" do  
+  it "is able to add and update reminder" do
     VCR.use_cassette("reminders_is_able_to_add_and_update_reminder") do
-      reminder_item = @client.sync_items.add({content: "ItemForReminderTest"}) 
+      reminder_item = @client.sync_items.add({content: "ItemForReminderTest"})
       @client.sync_items.collection
-      add_reminder = @client.sync_reminders.add({item_id: reminder_item.id, 
-        service: "email", type: "absolute", due_date_utc: "2018-03-24T23:59"})
-       expect(add_reminder).to be_truthy
+      add_reminder = @client.sync_reminders.add({item_id: reminder_item.id,
+        service: "email", type: "absolute", due: {string: "tomorrow"}})
+      expect(add_reminder).to be_truthy
       reminders_list =  @client.sync_reminders.collection
       queried_object = reminders_list[add_reminder.id]
+      binding.pry
       expect(queried_object.item_id).to eq(reminder_item.id)
-      expect(queried_object.service).to eq("email")
       queried_object.service = "mobile"
       @client.sync_reminders.update({id: queried_object.id, service: queried_object.service})
       reminders_list =  @client.sync_reminders.collection
       queried_object = reminders_list[queried_object.id]
       expect(queried_object.service).to eq("mobile")
-      
+
       @client.sync_items.delete([reminder_item])
       @client.sync_reminders.delete(add_reminder)
-      @client.sync  
-      
+      @client.sync
+
     end
   end
-  
+
 end
