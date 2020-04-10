2893d411d1568da9131d5f5bdd7cc7548c5b3ee7
Han-Shen Yuan <han.yuan@gmail.com>
2020-04-10 14:00:29 -0700

    Updated spec to remove deprecated field

--- spec/projects_spec.rb
+++ spec/projects_spec.rb
@@ -8,14 +8,14 @@ describe Todoist::Sync::Projects do
     config.cassette_library_dir = "fixtures/vcr_cassettes"
     config.hook_into :webmock
   end
-  
+
   before(:all) do
     Todoist::Util::Uuid.type = "projects"
-  end  
+  end
 
   before do
-    @client = load_client  
-   
+    @client = load_client
+
   end
 
   it "is able to get projects" do
@@ -38,16 +38,17 @@ describe Todoist::Sync::Projects do
     end
   end
 
-  it "is able to update a project" do  
-    VCR.use_cassette("projects_is_able_to_update_a_project") do    
+  it "is able to update a project" do
+    VCR.use_cassette("projects_is_able_to_update_a_project") do
       update_project = @client.sync_projects.add({name: "Project4"})
-      update_project.indent = 2
-      result = @client.sync_projects.update(id: update_project.id, indent: update_project.indent)
+      update_project.name = "Project4Update"
+      result = @client.sync_projects.update(id: update_project.id, name: update_project.name)
       expect(result).to be_truthy
       queried_object = @client.sync_projects.collection[update_project.id]
-      expect(queried_object.indent).to eq(2)
+      binding.pry
+      expect(queried_object.name).to eq(update_project.name)
       @client.sync_projects.delete([update_project])
-      @client.sync      
+      @client.sync
     end
   end
 
@@ -56,43 +57,43 @@ describe Todoist::Sync::Projects do
       project = @client.sync_projects.add({name: "Project1"})
       expect(project).to be_truthy
       project2 = @client.sync_projects.add({name: "Project2"})
-  
+
       # Restore the projects fully
-  
+
       project_collection = @client.sync_projects.collection
-  
+
       project = project_collection[project.id]
       project2 = project_collection[project2.id]
-  
-  
+
+
       # Keep track of original values
       project_order = project.item_order
       project_order2 = project2.item_order
-  
+
       # Swap orders
       project.item_order = project_order2
       project2.item_order = project_order
-  
+
       # Indent project
       project.indent = 2
-  
+
       @client.sync_projects.update_multiple_orders_and_indents([project, project2])
       project_collection = @client.sync_projects.collection
-  
+
       # Check to make sure newly retrieved object values match old ones
-  
+
       expect(project_collection[project.id].item_order).to eq(project_order2)
       expect(project_collection[project2.id].item_order).to eq(project_order)
       expect(project_collection[project.id].indent).to eq(2)
-  
+
       # Clean up extra project
-  
+
       @client.sync_projects.delete([project2])
       @client.sync_projects.delete([project])
       @client.sync
     end
-    
+
 
   end
-  
+
 end
